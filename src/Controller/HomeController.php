db30e3eac96150ce12105ca5d754a9ca416d1087
scheikh <abdulrahman404@hotmail.com>
2025-04-09 11:07:22 +0200

    install octrine and create databank

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function home(): Response
+    {
+        $starshipCount = 458;
+        return $this->render('home/home.html.twig', [
+            'numberOfStarships' => $starshipCount,
+        ]);
+        // return new Response('<stron>Home</stron>');
+    }
+}
