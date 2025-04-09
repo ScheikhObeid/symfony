f1f0792d8468aad48e1ef77df1315f32aefd1cbb
scheikh <abdulrahman404@hotmail.com>
2025-04-09 15:13:05 +0200

    Creating CRUD Service, add Menu to base.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home')]
+    #[Route('/', name: 'app_home')]
     public function home(): Response
     {
         $starshipCount = 458;
