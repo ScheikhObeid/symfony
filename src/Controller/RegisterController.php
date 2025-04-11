19cf9cd66e5eaceedad27ddf76af7b400df6eb2d
scheikh <abdulrahman404@hotmail.com>
2025-04-11 21:07:08 +0200

    Register method was created and getestet, the username and hashed password stored to the databank

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,18 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class RegisterController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(): Response
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
         $registerForm = $this->createFormBuilder();
         $registerForm 
@@ -30,6 +34,21 @@ final class RegisterController extends AbstractController
     
         $form = $registerForm->getForm(); // ✅ must call getForm() outside chain if $registerForm used
     
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            if ($form->isValid()) {
+                $input = $form->getData();
+                $user = new User();
+                $user->setUsername($input['username']);
+                $hashedPassword = $passwordHasher->hashPassword($user, $input['password']);
+                $user->setPassword($hashedPassword);
+                $entityManager->persist($user);
+                $entityManager->flush();
+            } else {
+                dump('invalid');
+            }
+        }
+
         return $this->render('register/index.html.twig', [
             'regform' => $form->createView(), // ✅ use $form here
             'controller_name' => 'RegisterController',
